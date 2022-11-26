94d4cc1f258f795d01b1128ee8414cac7ff93fea
Amin Charoliya <amincharoliya@gmail.com>
2022-11-27 00:10:11 +0530

    Update testimonial slider breakpoint

--- components/Home/Testimonials.js
+++ components/Home/Testimonials.js
@@ -11,11 +11,11 @@ const Testimonials = () => {
 			items: 3,
 		},
 		tablet: {
-			breakpoint: { max: 1024, min: 464 },
+			breakpoint: { max: 1024, min: 768 },
 			items: 2,
 		},
 		mobile: {
-			breakpoint: { max: 464, min: 0 },
+			breakpoint: { max: 767, min: 0 },
 			items: 1,
 		},
 	};

5fe6d5d5f90744dd66301bd7569f0139193658e5
Amin Charoliya <amincharoliya@gmail.com>
2022-11-27 00:08:53 +0530

    Add Blog List component reading data from md files

---
+++ components/Home/BlogList.js
@@ -0,0 +1,41 @@
+/* eslint-disable @next/next/no-img-element */
+import Link from 'next/link';
+
+const BlogList = ({ posts }) => {
+	console.log('path', posts);
+	return (
+		<div className="lg:container px-5 py-14">
+			<h2 className="text-2xl font-bold text-heading-light dark:text-heading-dark leading-tight mb-16 md:text-3xl lg:text-4xl md:pt-5 text-center">
+				Blog
+			</h2>
+			<div className="flex flex-col md:flex-row">
+				{posts.map((item) => (
+					<div
+						key={item.slug}
+						className="w-full md:w-1/3 mb-12 md:mx-3 bg-white dark:bg-slate-800 shadow rounded-sm"
+					>
+						<Link href={`/blogs/${item.slug}`}>
+							<img
+								src={`/images/articles/${item.frontmatter.bannerImage}`}
+								alt={item.frontmatter.title}
+								className="w-full rounded-t-sm h-52 object-cover"
+							/>
+						</Link>
+						<div className="p-4">
+							<h3 className="text-lg font-bold text-heading-light dark:text-heading-dark mb-2">
+								<Link href={`/blogs/${item.slug}`}>
+									{item.frontmatter.title}
+								</Link>
+							</h3>
+							<p className="text-sm">
+								{item.frontmatter.description}
+							</p>
+						</div>
+					</div>
+				))}
+			</div>
+		</div>
+	);
+};
+
+export default BlogList;
--- pages/index.js
+++ pages/index.js
@@ -2,13 +2,26 @@ import Layout from '../components/Layout';
 import Header from '../components/Header';
 import Banner from '../components/Home/Banner';
 import Testimonials from '../components/Home/Testimonials';
+import BlogList from '../components/Home/BlogList';
+import { getAllPosts } from '../lib/blog/posts';
 
-export default function Home() {
+export default function Home({ posts }) {
 	return (
 		<Layout>
 			<Header />
 			<Banner />
 			<Testimonials />
+			<BlogList posts={posts} />
 		</Layout>
 	);
+}
+
+export async function getStaticProps() {
+	const posts = getAllPosts();
+
+	return {
+		props: {
+			posts,
+		},
+	};
 }

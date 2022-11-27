cbd1f0a657a4422bac7165bdb806d25dbbbbceb8
Amin Charoliya <amincharoliya@gmail.com>
2022-11-27 15:01:20 +0530

    Animate on theme change

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,18 +1,30 @@
+import clsx from 'clsx';
 import { useTheme } from 'next-themes';
+import { useState } from 'react';
 import { Moon, Sun } from '../utils/Icons';
 
 const ThemeSwitch = ({ classes }) => {
 	const { theme, setTheme } = useTheme();
+	const [animation, setAnimation] = useState(false);
 	const ToggleTheme = () => {
 		if (theme == 'dark') {
 			setTheme('light');
 		} else {
 			setTheme('dark');
 		}
+		setAnimation(true);
+		setTimeout(() => {
+			setAnimation(false);
+		}, 300);
 	};
+
 	return (
 		<button className={classes} onClick={() => ToggleTheme()}>
-			{theme == 'dark' ? <Moon /> : <Sun />}
+			{theme == 'dark' ? (
+				<Moon classes={clsx(animation && 'animate-ping')} />
+			) : (
+				<Sun classes={clsx(animation && 'animate-ping')} />
+			)}
 		</button>
 	);
 };

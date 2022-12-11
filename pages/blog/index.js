34c0155ead28b8ecfa839c22907ce492f5a12f97
Amin Charoliya <amincharoliya@gmail.com>
2022-12-11 19:27:20 +0530

    Filter posts if tags are available in URL params

--- pages/blog/index.js
+++ pages/blog/index.js
@@ -1,17 +1,45 @@
-import Layout from '../../components/Layout';
-import BlogList from '../../components/Home/BlogList';
+import { useRouter } from 'next/router';
 
+import Layout from '../../components/Layout';
 import { getAllPosts } from '../../lib/blog/posts';
 import PostListing from '../../components/PostListing';
+import { XMark } from '../../utils/Icons';
 
 const Blog = ({ posts }) => {
+	const router = useRouter();
+	const { query } = router;
+
+	const tags = query.tags;
+
+	// Filter the posts based on the tags value if available or just return the posts array
+	const filteredPosts = tags
+		? posts.filter((post) => post.frontmatter.tags.includes(tags))
+		: posts;
+
+	const handleClearTags = () => {
+		// Navigate to the same page without the "tags" query parameter
+		router.push({
+			pathname: router.pathname,
+			query: { ...router.query, tags: undefined },
+		});
+	};
+
 	return (
 		<Layout>
 			<section className="lg:container px-5 py-14" id="blog">
 				<h2 className="text-2xl font-bold text-heading-light dark:text-heading-dark leading-tight mb-16 md:text-3xl lg:text-4xl md:pt-5 text-center">
 					Blog
 				</h2>
-				<PostListing posts={posts} />
+				{tags && (
+					<button
+						onClick={handleClearTags}
+						className="inline-block text-theme border border-theme mb-6 hover:bg-theme hover:text-content-dark focus:border-transparent dark:focus:border-transparent focus:outline-none focus:ring-2 focus:ring-theme font-medium rounded-lg text-sm px-4 py-2 text-center duration-200"
+					>
+						Clear Filter <XMark classes="inline-block" size={20} />
+					</button>
+				)}
+
+				<PostListing posts={filteredPosts} />
 			</section>
 		</Layout>
 	);
--- utils/Icons.js
+++ utils/Icons.js
@@ -198,3 +198,24 @@ export function LinkedIn({ size = 30, classes = '' }) {
 		</svg>
 	);
 }
+
+export function XMark({ size = 30, classes = '' }) {
+	return (
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			fill="none"
+			viewBox="0 0 24 24"
+			strokeWidth={1.5}
+			stroke="currentColor"
+			className={classes}
+			height={size}
+			width={size}
+		>
+			<path
+				strokeLinecap="round"
+				strokeLinejoin="round"
+				d="M6 18L18 6M6 6l12 12"
+			/>
+		</svg>
+	);
+}
